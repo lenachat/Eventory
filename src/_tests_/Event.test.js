813dd949b375df45fe6041af254a787cd71e98e6
lenachat <124374456+lenachat@users.noreply.github.com>
2024-08-20 14:53:34 +0200

    Implement show/hide event details feature
    - Added the ability to toggle event details visibility in the Event component.
    - Added a button that toggles between "Show Details" and "Hide Details" based on the state.
    - Implemented unit tests to ensure:
      - The event details section is hidden by default.
      - The event details are displayed when "Show Details" is clicked.
      - The button text changes to "Hide Details" when details are shown.

---
+++ src/_tests_/Event.test.js
@@ -0,0 +1,87 @@
+import { render, screen } from '@testing-library/react';
+import Event from '../components/Event';
+import { getEvents } from '../api';
+import userEvent from '@testing-library/user-event';
+
+describe('<Event /> component', () => {
+  let mockEvent;
+  beforeAll(async () => {
+    const allEvents = await getEvents();
+    mockEvent = allEvents[0];
+  });
+
+  let EventComponent;
+  beforeEach(() => {
+    EventComponent = render(<Event event={mockEvent} />);
+  });
+
+  test('renders event component', () => {
+    const eventElement = EventComponent.getByRole('listitem');
+    expect(eventElement).toHaveClass('event-item');
+  });
+
+  test('renders event title', () => {
+    const eventTitle = screen.getByText(mockEvent.summary);
+    expect(eventTitle).toBeInTheDocument();
+  });
+
+  test('renders event start time', () => {
+    const eventStartTime = screen.getByText(mockEvent.created);
+    expect(eventStartTime).toBeInTheDocument();
+  });
+
+  test('renders event location', () => {
+    const eventLocation = screen.getByText(mockEvent.location);
+    expect(eventLocation).toBeInTheDocument();
+  });
+
+  test('renders show details button', () => {
+    const showDetailsButton = screen.getByText('Show Details');
+    expect(showDetailsButton).toBeInTheDocument();
+  });
+
+  test('by default event details section should be hidden', () => {
+    // Check that the details are not visible by default
+    const eventDetails = screen.queryByText("Details");
+    expect(eventDetails).not.toBeInTheDocument();
+
+    // Ensure that the "Show Details" button is present
+    const showDetailsButton = screen.getByText('Show Details');
+    expect(showDetailsButton).toBeInTheDocument();
+  });
+
+  test('shows details when "Show Details" is clicked', async () => {
+    const user = userEvent.setup();
+    // Click the "Show Details" button
+    const showDetailsButton = screen.getByText('Show Details');
+    await user.click(showDetailsButton);
+
+    // Check that the details are visible
+    const eventDetails = screen.queryByText("Details");
+    expect(eventDetails).toBeInTheDocument();
+
+    // Verify the button text is now "Hide Details"
+    const hideDetailsButton = screen.getByText('Hide Details');
+    expect(hideDetailsButton).toBeInTheDocument();
+  });
+
+  test('hides details when "Hide Details" is clicked', async () => {
+    const user = userEvent.setup();
+    // Click the "Show Details" button
+    const showDetailsButton = screen.getByText('Show Details');
+    await user.click(showDetailsButton);
+
+    const hideDetailsButton = screen.queryByText('Hide Details');
+    expect(hideDetailsButton).toBeInTheDocument();
+
+    await user.click(hideDetailsButton);
+    // Check that the details are not visible
+    const eventDetails = screen.queryByText("Details");
+    expect(eventDetails).not.toBeInTheDocument();
+
+    // Verify the button text is now "Show Details"
+    const showDetailsButtonAfterHide = screen.getByText('Show Details');
+    expect(showDetailsButtonAfterHide).toBeInTheDocument();
+  });
+
+});
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,7 +1,24 @@
-const Event = () => {
-  return (
-    <li>
+import { useState } from 'react';
 
+const Event = ({ event }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  return (
+    <li className="event-item">
+      <h2> {event.summary} </h2>
+      <p> {event.created} </p>
+      <p> {event.location} </p>
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? 'Hide Details' : 'Show Details'}
+      </button>
+
+      {showDetails ?
+        <div>
+          <h3>Details</h3>
+          <p> {event.description} </p>
+        </div>
+        : null
+      }
     </li>
   );
 }
