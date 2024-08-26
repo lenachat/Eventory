7bf537470bcd8bf4d5c0011e934f85ffe4def070
lenachat <124374456+lenachat@users.noreply.github.com>
2024-08-26 18:41:54 +0200

    enable offline usage
    - caching the list of events to enable offline usage of the app

--- src/api.js
+++ src/api.js
@@ -42,6 +42,13 @@ export const getEvents = async () => {
   if (window.location.href.startsWith('http://localhost')) {
     return mockData;
   }
+
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events ? JSON.parse(events) : [];
+  }
+
   const token = await getAccessToken();
 
   if (token) {
@@ -50,6 +57,8 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
